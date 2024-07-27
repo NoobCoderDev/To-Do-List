98910f7bff4ef5b4fbaec6e799d27a60d1a2c225
Devanshu Sonbhurra <sonbhurra@gmail.com>
2024-07-27 20:52:39 +0530

    Updated

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { findDOMNode } from 'react-dom';
 
 function App() {
+  let [ToDoList,setToDoList] = useState([]);
+
+  let saveToDoList = (event)=>{
+
+    let todoName = event.target.todoName.value;
+    if(!ToDoList.includes(todoName)){
+      let finalToDoList = [...ToDoList,todoName];
+      setToDoList(finalToDoList);
+    }
+    else{
+      alert("Item already exists...")
+    }
+    event.preventDefault();
+  }
+
+  let list = ToDoList.map((value,index)=>{
+    
+    return(
+      <ToDoListItems value={value} key={index} indexNumber = {index} ToDoList={ToDoList} setToDoList={setToDoList}/>
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do-List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type='text' name='todoName'></input>
+        <button>Save</button>
+      </form>
+
+      <div className='outerDiv'>
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({value, indexNumber, ToDoList, setToDoList}) {
+
+  let [status,setStatus] = useState(false);
+  let deleteRow=()=>{
+    let finalData = ToDoList.filter((v,i)=>i!=indexNumber)
+    // console.log(finalData);
+    setToDoList(finalData);
+  }
+
+  let checkStatus = ()=>{
+    setStatus(!status);
+  }
+  return(
+    <li className={status ? 'completeToDo' : ''} onClick={checkStatus}>{indexNumber+1}. {value}<span onClick={deleteRow}>&times;</span></li>
+  )
+}
